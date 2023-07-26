35ffbc4b42e07e40cc2cec1faa001eff91d7816b
subeomi <tnqjarnqja@gmail.com>
2023-07-26 10:16:23 +0900

    Initial commit

---
+++ src/main/java/com/project/wekiosk/WekioskApplication.java
@@ -0,0 +1,13 @@
+package com.project.wekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WekioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wekiosk/WekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
